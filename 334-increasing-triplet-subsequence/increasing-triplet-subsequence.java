42df584c87922c58ad85fcfa517684763fc26976
Sarthak Pandey <157343205+Sar1702@users.noreply.github.com>
2026-01-02 12:31:28 +0530

    Time: 2 ms (98.85%) | Memory: 122.6 MB (55.91%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE , second = Integer.MAX_VALUE;
+        int n = nums.length;
+        for(int i = 0 ; i < n ; i++){
+            if( nums[i]<= first){
+                first = nums[i] ; 
+            }
+            else if(nums[i]<=second){
+                second = nums[i] ; 
+            }
+            else{
+                return true;
+            }
+
+        }
+       return false; 
+    }
+}
