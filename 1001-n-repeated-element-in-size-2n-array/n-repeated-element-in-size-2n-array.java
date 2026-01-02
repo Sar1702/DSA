cba35c027b61d3cc06278ac0f83189c64e7915c8
Sarthak Pandey <157343205+Sar1702@users.noreply.github.com>
2026-01-02 12:52:40 +0530

    Time: 2 ms (54.01%) | Memory: 47.5 MB (50.87%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,11 +1,18 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        int n = nums.length - 1;
-        if(nums[0] == nums[n]) return nums[0];
-        for(int i = 0 ;i< n; i++){
-            if(nums[i]== nums[i+1] || nums[i] == nums[i+2]) return nums[i];
-            
+        int n = nums.length/2;
+        for(int i = 0 ; i < nums.length ; i++){
+            int count = 0;
+            for(int j = 0 ; j<nums.length ; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+
+            }
+            if(count == n){
+                return nums[i];
+            }
         }
-        return nums[0];
+        return -1;
     }
 }
