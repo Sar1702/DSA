6f5d62e8ab68b8b44454f030a9267f4094ce65a0
Sarthak Pandey <157343205+Sar1702@users.noreply.github.com>
2026-09-04 19:33:06 +0530

    Time: 1264 ms (43.40%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 select p.product_name, s.year, s.price from Sales as s 
-join Product as p where p.product_id = s.product_id;
+join Product as p on p.product_id = s.product_id;
